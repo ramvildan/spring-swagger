93ec4c7aab12f3c36d2b813df689eb9a33af5212
Ramil Vildanov <ramvildan.work@gmail.com>
2023-02-16 18:09:24 +0300

    Swagger works

--- src/main/java/com/javamaster/springswagger/config/SpringFoxConfig.java
+++ src/main/java/com/javamaster/springswagger/config/SpringFoxConfig.java
@@ -2,6 +2,8 @@ package com.javamaster.springswagger.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -9,6 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableWebMvc
 @EnableSwagger2
 public class SpringFoxConfig {
 
@@ -19,5 +22,10 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
     }
 }
